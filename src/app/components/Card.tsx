71cf1a72a1a0f09c8234af6414d043b98e4b77bc
riittagerhards <rgerhards@outlook.com>
2022-01-26 10:49:20 +0100

    🎨 Finish Card component and optimize assets

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-function MemoryCard(): JSX.Element {
-  return <div className={styles.card}></div>;
+type MemoryCardProps = {
+  src: string;
+};
+
+function MemoryCard({ src }: MemoryCardProps): JSX.Element {
+  return (
+    <article className={styles.card}>
+      <img src={src} alt="" />
+    </article>
+  );
 }
 
 export default MemoryCard;
