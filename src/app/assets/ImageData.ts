fe6e6fd87f3295d8f896b10682282bf7f8456143
riittagerhards <rgerhards@outlook.com>
2022-01-26 12:31:22 +0100

    ✨ Add ImageData and simple map for the images in Game.tsx

---
+++ src/app/assets/ImageData.ts
@@ -0,0 +1,21 @@
+import Donut1 from './Donut1.jpg';
+import Donut2 from './Donut2.jpg';
+import Donut3 from './Donut3.jpg';
+import Donut4 from './Donut4.jpg';
+import Icecream1 from './Icecream1.jpg';
+import Icecream2 from './Icecream2.jpg';
+import Icecream3 from './Icecream3.jpg';
+import Icecream4 from './Icecream4.jpg';
+
+export const Images = [
+  { src: Donut1, key: 'image1' },
+  { src: Donut2, key: 'image2' },
+  { src: Donut3, key: 'image3' },
+  { src: Donut4, key: 'image4' },
+  { src: Icecream1, key: 'image5' },
+  { src: Icecream2, key: 'image6' },
+  { src: Icecream3, key: 'image7' },
+  { src: Icecream4, key: 'image8' },
+];
+
+export default Images;
--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './Game.module.css';
 import MemoryCard from './Card';
+import Images from '../assets/ImageData';
 
 function Game(): JSX.Element {
   return (
     <div className={styles.game}>
       <h1> Lets play card memory 🔖</h1>
-      <MemoryCard />
+      <div className={styles.cards}>
+        {Images.map((image) => (
+          <MemoryCard src={image.src} key={image.key} />
+        ))}
+      </div>
     </div>
   );
 }
