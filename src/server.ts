6dce77b423268ff63862c7e80f2eed011d6b8198
riittagerhards <rgerhards@outlook.com>
2022-02-10 18:43:34 +0100

    👷 Adjust server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (_request, response) => {
-  response.send('Hello World!');
+//app.get('/', (_request, response) => {
+//  response.send('Hello World!');
+//});
+
+app.get('*', (_request, response) => {
+  response.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(port, () => {
